678a48404b43165936ea25df9d92e437cda3f6c3
Karol Horosin <karol.horosin@gmail.com>
2024-01-06 17:12:50 +0100

    Update test prompts

--- test_local.sh
+++ test_local.sh
@@ -1,2 +1,3 @@
-curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{ "body": "{ \"prompt\": \"Generate a good name for a bakery.\" }" }'
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" \
+    -d '{ "body": "{ \"prompt\": \"Generate a good name for a bakery.\" }" }'
 
--- test_remote.sh
+++ test_remote.sh
@@ -1,4 +1,5 @@
 PROMPT="Create five questions for a job interview for a senior python software engineer position."
+# PROMPT="Generate a good name for a bakery."
 
 curl $LAMBDA_URL -d "{ \"prompt\": \"$PROMPT\" }" \
     | jq -r '.choices[0].text, .usage'
